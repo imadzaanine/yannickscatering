b96c0e73d0fa363e1f4e3435cbec69c44428041f
imad <imadzaanine@gmail.com>
2026-08-01 13:58:18 +0200

    Finished the arrow animation

--- public/components/layout/HeaderSection.tsx
+++ public/components/layout/HeaderSection.tsx
@@ -6,18 +6,25 @@ import Image from 'next/image';
 
 const HeaderSection: React.FC = () => {
   const { scrollY } = useScroll();
-  const opacity = useTransform(scrollY, [0, 200], [1, 0]);
-  const scale = useTransform(scrollY, [0, 200], [1, 0.6]);
-  const y = useTransform(scrollY, [0, 200], [0, -40]);
+  const logoOpacity = useTransform(scrollY, [0, 200], [1, 0]);
+  const logoScale = useTransform(scrollY, [0, 200], [1, 0.6]);
+  const logoY = useTransform(scrollY, [0, 200], [0, -40]);
+
+  const arrowOpacity = useTransform(scrollY, [0, 100], [1, 0]);
 
   return (
     <div className='flex flex-col items-center min-h-screen justify-between gap-4 pb-10'>
-      <motion.div style={{ opacity, scale, y }} className='sticky top-0'>
+      <motion.div style={{ opacity: logoOpacity, scale: logoScale, y: logoY }} className='sticky top-0'>
         <Logo width={250} height={40} />
       </motion.div>
-      <div>
+
+      <motion.div
+        style={{ opacity: arrowOpacity }}
+        animate={{ y: [0, -12, 0] }}
+        transition={{ duration: 1.4, repeat: Infinity, ease: 'easeInOut' }}
+      >
         <Image src="/Arrow 1.svg" alt='Arrow' width={25} height={0} />
-      </div>
+      </motion.div>
     </div>
   );
 };
